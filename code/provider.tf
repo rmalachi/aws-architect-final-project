f1849d35f9c3b7fe72df87423a877d0fe749e475
rmalachi <malachiron@gmail.com>
2024-10-05 01:00:16 +0300

    Update provider.tf

--- code/provider.tf
+++ code/provider.tf
@@ -1,5 +1,10 @@
 provider "aws" {
     region                    = "us-east-1"
-    alias                     = "account_route53"
+    shared_credentials_files  = [var.shared_credentials_files]
+}
+
+provider "aws" {
+    region                    = "eu-central-1"
+    alias                     = "eu_env"
     shared_credentials_files  = [var.shared_credentials_files]
 }
