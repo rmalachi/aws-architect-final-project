b7f50ddaf675406ca1b44d09cb575398385e7e1f
rmalachi <malachiron@gmail.com>
2024-10-05 16:07:46 +0300

    Fix #1 according Yuvals scheme

--- code/main.tf
+++ code/main.tf
@@ -2,7 +2,7 @@
 # This file describes the network infrastructure components:
 #     vpc, subnets, internet gateway, nat gateway, route tables
 #
-
+#
 # -------------------------------------------------------
 # Start US Infra Section
 # -------------------------------------------------------
@@ -10,48 +10,96 @@ resource "aws_vpc" "vpc" {
     cidr_block              = var.vpc_cidr
     enable_dns_hostnames    = true
     enable_dns_support      = true
-}
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id                  = aws_vpc.vpc.id
+    tags = {
+        Name = "vpc-us-10.0.0.0/16"
+    }
 }
-
-resource "aws_nat_gateway" "ngw" {
-    allocation_id           = aws_eip.nateip.id
-    subnet_id               = aws_subnet.public_subnet_1.id
-    depends_on              = [ aws_internet_gateway.igw ]
-}
-
-resource "aws_eip" "nateip" {
-    domain                  = "vpc"
-}
-
-resource "aws_subnet" "private_subnet_1" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.private_subnet_1
-    availability_zone       = var.availibilty_zone_1
-}
-
-resource "aws_subnet" "private_subnet_2" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.private_subnet_2
-    availability_zone       = var.availibilty_zone_2
-}
-
 resource "aws_subnet" "public_subnet_1" {
     vpc_id                  = aws_vpc.vpc.id
     cidr_block              = var.public_subnet_1
     availability_zone       = var.availibilty_zone_1
     map_public_ip_on_launch = true
-}
 
+    tags = {
+        Name = "public-subnet-1-10.0.1.0/24"
+    }
+}
+resource "aws_subnet" "private_subnet_1" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.private_subnet_1
+    availability_zone       = var.availibilty_zone_1
+
+    tags = {
+        Name = "private_subnet_us-10.0.2.0/24"
+    }
+}
+resource "aws_subnet" "private_sub_3_it" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.private_sub_3_it
+    availability_zone       = var.availibilty_zone_1
+
+    tags = {
+        Name = "private_sub_3_it-10.0.3.0/23"
+    }
+}
+resource "aws_subnet" "private_sub_4_rd" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.private_sub_4_rd
+    availability_zone       = var.availibilty_zone_1
+
+    tags = {
+        Name = "private_sub_4_rd-10.0.5.0/22"
+    }
+}
 resource "aws_subnet" "public_subnet_2" {
     vpc_id                  = aws_vpc.vpc.id
     cidr_block              = var.public_subnet_2
     availability_zone       = var.availibilty_zone_2
     map_public_ip_on_launch = true
-}
 
+    tags = {
+        Name = "public-subnet-2-10.0.3.0/24"
+    }
+}
+resource "aws_subnet" "private_subnet_2" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.private_subnet_2
+    availability_zone       = var.availibilty_zone_2
+
+    tags = {
+        Name = "private_subnet_us-10.0.4.0/24"
+    }
+}
+resource "aws_subnet" "private_sub_13_it" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.private_sub_13_it
+    availability_zone       = var.availibilty_zone_2
+
+    tags = {
+        Name = "private_sub_13_it-10.0.13.0/23"
+    }
+}
+resource "aws_subnet" "private_sub_14_rd" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.private_sub_14_rd
+    availability_zone       = var.availibilty_zone_2
+
+    tags = {
+        Name = "private_sub_14_rd-10.0.15.0/22"
+    }
+}
+resource "aws_internet_gateway" "igw" {
+    vpc_id                  = aws_vpc.vpc.id
+}
+resource "aws_nat_gateway" "ngw" {
+    allocation_id           = aws_eip.nateip.id
+    subnet_id               = aws_subnet.public_subnet_1.id
+    depends_on              = [ aws_internet_gateway.igw ]
+}
+resource "aws_eip" "nateip" {
+    domain                  = "vpc"
+}
 # -------------------------------------------------------
 # Start EU Infra Section
 # -------------------------------------------------------
@@ -60,42 +108,89 @@ resource "aws_vpc" "vpc-eu" {
     enable_dns_hostnames    = true
     enable_dns_support      = true
     provider                = aws.eu_env
+    
+    tags = {
+        Name = "vpc-eu-192.168.0.0/16"
+    }
 }
+resource "aws_subnet" "public_sub_1" {
+    vpc_id                  = aws_vpc.vpc-eu.id
+    cidr_block              = var.public_subnet_eu_1
+    availability_zone       = var.availibilty_zone_eu_1
+    map_public_ip_on_launch = true
 
-resource "aws_subnet" "private_subnet_eu_1" {
+    tags = {
+        Name = "public-sub-1-192.168.1.0/24"
+    }
+}
+resource "aws_subnet" "private_sub_2" {
     vpc_id                  = aws_vpc.vpc-eu.id
     cidr_block              = var.private_subnet_1
-    availability_zone       = var.availibilty_zone_1
-    provider                = aws.eu_env
-}
+    availability_zone       = var.availibilty_zone_eu_1
 
-resource "aws_subnet" "private_subnet_eu_2" {
+    tags = {
+        Name = "private_sub_2-192.168.2.0/24"
+    }
+}
+resource "aws_subnet" "private_sub_3_it" {
     vpc_id                  = aws_vpc.vpc-eu.id
     cidr_block              = var.private_subnet_2
-    availability_zone       = var.availibilty_zone_2
-    provider                = aws.eu_env
-}
+    availability_zone       = var.availibilty_zone_eu_1
 
-resource "aws_subnet" "public_subnet_eu_1" {
-    vpc_id                  = aws_vpc.vpc-eu.id
-    cidr_block              = var.public_subnet_1
-    availability_zone       = var.availibilty_zone_1
-    map_public_ip_on_launch = true
-    provider                = aws.eu_env
+    tags = {
+        Name = "private_sub_3_it-192.168.3.0/23"
+    }
 }
-
-resource "aws_subnet" "public_subnet_eu_2" {
+resource "aws_subnet" "private_sub_4_rd" {
     vpc_id                  = aws_vpc.vpc-eu.id
-    cidr_block              = var.public_subnet_2
-    availability_zone       = var.availibilty_zone_2
+    cidr_block              = var.private_subnet_1
+    availability_zone       = var.availibilty_zone_eu_1
+
+    tags = {
+        Name = "private_sub_4_rd-192.168.5.0/22"
+    }
+}
+resource "aws_subnet" "public_sub_11" {
+    vpc_id                  = aws_vpc.vpc-eu.id
+    cidr_block              = var.public_subnet_eu_1
+    availability_zone       = var.availibilty_zone_eu_2
     map_public_ip_on_launch = true
-    provider                = aws.eu_env
+
+    tags = {
+        Name = "public-sub-11-192.168.11.0/24"
+    }
+}
+resource "aws_subnet" "private_sub_12" {
+    vpc_id                  = aws_vpc.vpc-eu.id
+    cidr_block              = var.public_subnet_eu_11
+    availability_zone       = var.availibilty_zone_eu_2
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "private_sub_12-192.168.12.0/24"
+    }
+}
+resource "aws_subnet" "private_sub_13_it" {
+    vpc_id                  = aws_vpc.vpc-eu.id
+    cidr_block              = var.private_subnet_2
+    availability_zone       = var.availibilty_zone_eu_2
+
+    tags = {
+        Name = "public-sub-13-192.168.13.0/23"
+    }
+}
+resource "aws_subnet" "private_sub_14_rd" {
+    vpc_id                  = aws_vpc.vpc-eu.id
+    cidr_block              = var.private_subnet_1
+    availability_zone       = var.availibilty_zone_eu_2
+
+    tags = {
+        Name = "private_sub_14_rd-192.168.15.0/22"
+    }
 }
 # -------------------------------------------------------
 # End Infra EU Section
 # -------------------------------------------------------
-
-
 resource "aws_route_table" "public" {
     vpc_id                  = aws_vpc.vpc.id
     
